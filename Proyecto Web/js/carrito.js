fb280e0a881efdd7920c3be71b7d66de665579ca
71731557 <112639708+71731557@users.noreply.github.com>
2022-11-20 09:20:04 -0500

    Add files via upload

---
+++ Proyecto Web/js/carrito.js
@@ -0,0 +1,170 @@
+const shopContent = document.getElementById("bloque-1");
+const shopContent2 = document.getElementById("bloque-2");
+const shopContent3 = document.getElementById("bloque-3");
+const shopContent4 = document.getElementById("bloque-4");
+const verCarrito = document.getElementById("verCarrito");
+const modalContainer = document.getElementById("modal-container");
+const cantidadCarrito = document.getElementById("cantidadCarrito");
+
+
+let carrito = [];
+
+productosNike.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "col-xs-12 col-sm-12 col-md-4 img-fluid product"
+    content.innerHTML = `
+        <img class= "product__img" src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="product__price">${product.precio}$</p>
+    `;
+
+    shopContent.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.className = "btn btn-block btn-primary agregar-carrito";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () =>{
+        const repeat  = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        } else {
+            carrito.push({
+                id : product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            });
+        }
+        console.log(carrito);
+        carritoCounter();
+    })
+});
+
+productosAdidas.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "col-xs-12 col-sm-12 col-md-4 img-fluid product"
+    content.innerHTML = `
+        <img class= "product__img" src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="product__price">${product.precio}$</p>
+    `;
+
+    shopContent2.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.className = "btn btn-block btn-primary agregar-carrito";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () =>{
+        const repeat  = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        } else {
+            carrito.push({
+                id : product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            });
+        }
+        console.log(carrito);
+        carritoCounter();
+    })
+})
+
+productosGucci.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "col-xs-12 col-sm-12 col-md-4 img-fluid product"
+    content.innerHTML = `
+        <img class= "product__img" src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="product__price">${product.precio}$</p>
+    `;
+
+    shopContent3.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.className = "btn btn-block btn-primary agregar-carrito";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () =>{
+        const repeat  = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        } else {
+            carrito.push({
+                id : product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            });
+        }
+        console.log(carrito);
+        carritoCounter();
+    })
+})
+
+productosJordan.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "col-xs-12 col-sm-12 col-md-4 img-fluid product"
+    content.innerHTML = `
+        <img class= "product__img" src="${product.img}">
+        <h3>${product.nombre}</h3>
+        <p class="product__price">${product.precio}$</p>
+    `;
+
+    shopContent4.append(content);
+
+    let comprar = document.createElement("button")
+    comprar.className = "btn btn-block btn-primary agregar-carrito";
+    comprar.innerText = "comprar";
+
+    content.append(comprar);
+
+    comprar.addEventListener("click", () =>{
+
+        const repeat  = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+        if(repeat){
+            carrito.map((prod) => {
+                if(prod.id === product.id){
+                    prod.cantidad++;
+                }
+            });
+        } else {
+            carrito.push({
+                id : product.id,
+                img: product.img,
+                nombre: product.nombre,
+                precio: product.precio,
+                cantidad: product.cantidad,
+            });
+        }
+        console.log(carrito);
+        carritoCounter();
+    });
+});
+
+
+
---
+++ Proyecto Web/js/productos.js
@@ -0,0 +1,182 @@
+const productosNike = [
+    {
+        id: 1 ,
+        nombre: "Polo Negro Nike" ,
+        precio: 150,
+        img: "Img/nike-1.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 2 ,
+        nombre: "Polo Blanco Nike" ,
+        precio: 180,
+        img: "Img/nike-2.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 3 ,
+        nombre: "Polo Camisero Nike" ,
+        precio: 175,
+        img: "Img/nike-3.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 4 ,
+        nombre: "Buso deportivo Nike" ,
+        precio: 125,
+        img: "Img/nike-4.webp",
+        cantidad: 1,
+    },
+    {
+        id: 5 ,
+        nombre: "Pantalon Griss Nike" ,
+        precio: 220,
+        img: "Img/nike-5.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 6 ,
+        nombre: "Buso color negro Nike" ,
+        precio: 140,
+        img: "Img/nike-6.webp",
+        cantidad: 1,
+    },
+]
+
+const productosAdidas = [
+    {
+        id: 7 ,
+        nombre: "Polo Negro Adidas" ,
+        precio: 230,
+        img: "Img/adidas-1.webp",
+        cantidad: 1,
+    },
+    {
+        id: 8 ,
+        nombre: "Polo Blanco Adidas" ,
+        precio: 160,
+        img: "Img/adidas-2.webp",
+        cantidad: 1,
+    },
+    {
+        id: 9 ,
+        nombre: "Polo Griss Adidas" ,
+        precio: 190,
+        img: "Img/adidas-3.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 10 ,
+        nombre: "Buso Negro CD Adidas" ,
+        precio: 200,
+        img: "Img/adidas-4.webp",
+        cantidad: 1,
+    },
+    {
+        id: 11 ,
+        nombre: "Buso Azul Adidas" ,
+        precio: 140,
+        img: "Img/adidas-5.webp",
+        cantidad: 1,
+    },
+    {
+        id: 12 ,
+        nombre: "Buso Negro SD Adidas" ,
+        precio: 177,
+        img: "Img/adidas-6.webp",
+        cantidad: 1,
+    },
+
+]
+
+const productosGucci = [
+    {
+        id: 13 ,
+        nombre: "Polo Camisero N Gucci" ,
+        precio: 255,
+        img: "Img/gucci-1.webp",
+        cantidad: 1,
+    },
+    {
+        id: 14 ,
+        nombre: "Polo Camisero CD Gucci" ,
+        precio: 246,
+        img: "Img/gucci-2.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 15 ,
+        nombre: "Polo N CD Gucci" ,
+        precio: 275,
+        img: "Img/gucci-3.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 16 ,
+        nombre: "Buso Negro Gucci" ,
+        precio: 185,
+        img: "Img/gucci-4.webp",
+        cantidad: 1,
+    },
+    {
+        id: 17 ,
+        nombre: "Buso Crema Gucci" ,
+        precio: 260,
+        img: "Img/gucci-5.webp",
+        cantidad: 1,
+    },
+    {
+        id: 18 ,
+        nombre: "Buso Marron Gucci" ,
+        precio: 178,
+        img: "Img/gucci-6.webp",
+        cantidad: 1,
+    },
+
+]
+
+const productosJordan = [
+    {
+        id: 19 ,
+        nombre: "Polo Blanco Jordan" ,
+        precio: 300,
+        img: "Img/jordan-1.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 20 ,
+        nombre: "Polo Griss Jordan" ,
+        precio: 241,
+        img: "Img/jordan-2.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 21 ,
+        nombre: "Polo Negro Jordan" ,
+        precio: 256,
+        img: "Img/jordan-3.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 22 ,
+        nombre: "Buso Negro Jordan" ,
+        precio: 275,
+        img: "Img/jordan-4.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 23 ,
+        nombre: "Buso Griss Jordan" ,
+        precio: 220,
+        img: "Img/jordan-5.jpg",
+        cantidad: 1,
+    },
+    {
+        id: 24 ,
+        nombre: "Pantalon Griss Oscuro Jordan" ,
+        precio: 286,
+        img: "Img/jordan-6.webp",
+        cantidad: 1,
+    },
+
+]
